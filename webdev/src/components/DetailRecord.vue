21f8353e93c4e0b5a528b47abdad176be3d1b869
Allen Lawrence <blue_sky1997@live.com>
2018-03-09 20:44:20 +0800

    Bug Fix Fixed the bug of date display in detail records component

--- webdev/src/components/DetailRecord.vue
+++ webdev/src/components/DetailRecord.vue
@@ -49,8 +49,29 @@ export default {
   },
 
   methods: {
+    formatDate (date, format) {
+      var o = {
+        'M+': date.getMonth() + 1,                     // 月份
+        'd+': date.getDate(),                          // 日
+        'h+': date.getHours(),                         // 小时
+        'm+': date.getMinutes(),                       // 分
+        's+': date.getSeconds(),                       // 秒
+        'q+': Math.floor((date.getMonth() + 3) / 3),   // 季度
+        'S': date.getMilliseconds()                    // 毫秒
+      }
+      if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+      }
+      for (var k in o) {
+        if (new RegExp('(' + k + ')').test(format)) {
+          format = format.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+        }
+      }
+      return format
+    },
     normalizedDate (date) {
-      return (new Date(date)).toLocaleString()
+      const dateObj = new Date(date)
+      return this.formatDate(dateObj, 'yyyy/MM/dd hh:mm:ss')
     }
   }
 
